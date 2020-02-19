cbf4c3ec462aabeb1e7035b6fc10ec087397aa19
Zane Selvans <zane.selvans@catalyst.coop>
2020-02-19 15:50:13 -0600

    Initial attempt at a FERC Form 1 DB release repo.

---
+++ ferc1-release.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# A script to generate a reproducible data release locally.
+# Assumes:
+#  - A Unix-like OS
+#  - libsnappy is installed
+#  - conda is installed
+#  - Environment variable $CONDA_EXE is path to conda
+#  - Should be run from within a fresh git clone:
+#    https://github.com/catalyst-cooperative/ferc1-data-release.git
+
+PUDL_VERSION=0.3.2
+START_TIME=$(date --iso-8601="seconds")
+FERC1_YEARS1=""
+
+###############################################################################
+# Create, activate, and record the ferc1-data-release conda environment
+###############################################################################
+echo "======================================================================"
+echo $START_TIME
+echo "Creating and archiving PUDL conda environment"
+echo "======================================================================"
+$CONDA_EXE init bash
+eval "$($CONDA_EXE shell.bash hook)"
+$CONDA_EXE env remove --name ferc1-data-release
+$CONDA_EXE create --yes --name ferc1-data-release \
+    --strict-channel-priority --channel conda-forge \
+    python=3.7 pip git catalystcoop.pudl=$PUDL_VERSION
+source activate ferc1-data-release
+
+ACTIVE_CONDA_ENV=$($CONDA_EXE env list | grep '\*' | awk '{print $1}')
+echo "Active conda env: $ACTIVE_CONDA_ENV"
+
+# Record exactly which software was installed for ETL:
+$CONDA_EXE env export | grep -v "^prefix" > archived-environment.yml
+
+echo "======================================================================"
+date --iso-8601="seconds"
+echo "Setting up PUDL data management environment."
+echo "======================================================================"
+pudl_setup --clobber ./
+
+echo "======================================================================"
+date --iso-8601="seconds"
+echo "Downloading raw input data."
+echo "======================================================================"
+pudl_data --sources ferc1 $FERC1_YEARS
+
+echo "======================================================================"
+date --iso-8601="seconds"
+echo "Cloning FERC Form 1 into SQLite."
+echo "======================================================================"
+ferc1_to_sqlite --clobber ferc1-release-settings.yml
+
+echo "======================================================================"
+date --iso-8601="seconds"
+echo "Archiving raw input data for distribution."
+echo "======================================================================"
+mkdir zenodo-archive
+tar -czf zenodo-archive/ferc1-input-data.tgz data/
+
+echo "======================================================================"
+date --iso-8601="seconds"
+echo "Archiving PUDL datapackages for distribution."
+echo "======================================================================"
+tar -czf zenodo-archive/ferc1-sqlite.tgz sqlite/ferc1.sqlite
+
+cp ferc1-release.sh \
+    reproduce-ferc1-release.sh \
+    ferc1-release-settings.yml \
+    archived-environment.yml \
+    README.md \
+    zenodo-archive
+
+echo "======================================================================"
+END_TIME=$(date --iso-8601="seconds")
+ARCHIVE_SIZE=$(du -sh zenodo-archive)
+echo "FERC Form 1 Database Cloning Complete."
+echo "START TIME:" $START_TIME
+echo "END TIME:  " $END_TIME
+echo "Archive Size:" $ARCHIVE_SIZE
+echo "======================================================================"
---
+++ reproduce-ferc1-release.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+###############################################################################
+# Create, activate, and record the pudl-data-release conda environment
+###############################################################################
+START_TIME=$(date --iso-8601="seconds")
+echo "======================================================================"
+echo $START_TIME
+echo "Creating and archiving PUDL conda environment"
+echo "======================================================================"
+$CONDA_EXE init bash
+eval "$($CONDA_EXE shell.bash hook)"
+$CONDA_EXE env remove --name reproduce-ferc1-data-release
+$CONDA_EXE config --set channel_priority strict
+$CONDA_EXE env create \
+    --name reproduce-ferc1-data-release \
+    --file archived-environment.yml
+source activate reproduce-ferc1-data-release
+
+ACTIVE_CONDA_ENV=$($CONDA_EXE env list | grep '\*' | awk '{print $1}')
+echo "Active conda env: $ACTIVE_CONDA_ENV"
+
+echo "======================================================================"
+date --iso-8601="seconds"
+echo "Setting up PUDL data management environment."
+echo "======================================================================"
+pudl_setup --clobber ./
+
+echo "======================================================================"
+date --iso-8601="seconds"
+echo "Extracting archived input data."
+echo "======================================================================"
+tar -xzf ferc1-input-data.tgz
+
+echo "======================================================================"
+date --iso-8601="seconds"
+echo "Cloning FERC Form 1 into SQLite."
+echo "======================================================================"
+ferc1_to_sqlite --clobber ferc1-release-settings.yml
+
+echo "======================================================================"
+END_TIME=$(date --iso-8601="seconds")
+echo "FERC Form 1 database release re-creation complete."
+echo "START TIME:" $START_TIME
+echo "END TIME:  " $END_TIME
+echo "======================================================================"
